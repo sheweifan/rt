e8ab917e5d59fe126ca1f36374750253fefcbcbb
weifan she <923986234@qq.com>
2018-03-01 09:45:06 +0800

    add

--- src/container/Product/Product.js
+++ src/container/Product/Product.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react'
+import { Tabs } from 'antd-mobile'
+import List from 'component/List/List'
+import ProductItem from 'component/ProductItem/ProductItem'
+import { searchZhaiProduct } from 'api'
+import './Product.styl'
+
+const tabs = [
+  { title: '直投项目' },
+  { title: '债券转让' },
+  { title: '新手专享' }
+]
 
 class Product extends Component {
+  constructor(props) {
+    super(props)
+
+    this.getData = this.getData.bind(this)
+  }
+  getData(index, size) {
+    return searchZhaiProduct({
+      mPageNow: index,
+      mPageSize: size,
+      type: 0
+    })
+      .then(data => {
+        const { mList, mPageCount, mPageNow } = data
+        return Promise.resolve({list: mList, pageCount: mPageCount, pageNow: mPageNow})
+      })
+  }
   render() {
     return (
-      <div>
-        Product
+      <div className="productContainer">
+        <Tabs
+          tabs={tabs}
+          useOnPan={false}
+          tabBarUnderlineStyle={{borderColor: '#ff6000'}}
+          tabBarActiveTextColor="#ff6000"
+          prerenderingSiblingsNumber={false}
+        >
+          <List
+            renderRow={(item) => <ProductItem {...item} />}
+            getData={this.getData}
+          />
+          <div>
+            Content of second tab
+          </div>
+          <div>
+            Content of third tab
+          </div>
+        </Tabs>
       </div>
     )
   }
