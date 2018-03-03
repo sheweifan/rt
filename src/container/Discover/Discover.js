50375ff5b770a261a64d8e44a4b3ef127c3e2ad6
dolor <923986234@qq.com>
2018-03-03 22:00:28 +0800

    add Discover

---
+++ src/container/Discover/Discover.js
@@ -0,0 +1,40 @@
+import React, { Component, PureComponent } from 'react'
+import './Discover.styl'
+
+const data = [{"img":"https://www.1-dt.com/phone/img/2018wxdogact_banner.jpg","url":""},{"img":"img/wap168_newss.jpg","url":""},{"img":"img/tuijianbanner.jpg","url":""},{"img":"https://www.1-dt.com/phone/img/wxdoubledenier_banner.jpg","url":""},{"img":"https://www.1-dt.com/phone/img/wxexperiment_bannertwo.jpg","url":""},{"img":"https://www.1-dt.com/phone/img/wxtwelve_banner.jpg","url":""},{"img":"https://www.1-dt.com/phone/img/meetluodi_banner.jpg","url":""},{"img":"img/wap168_newss.jpg","url":""},{"img":"https://www.1-dt.com/phone/img/wxsingles_banner.jpg","url":""},{"img":"https://www.1-dt.com/phone/img/shuagnjie_banner.jpg","url":""},{"img":"img/bookingiphone.jpg","url":""},{"img":"img/qixi.jpg","url":""},{"img":"img/groupInvestment.jpg","url":""},{"img":"img/twoyear_banner.jpg","url":""},{"img":"https://www.1-dt.com/phone/img/0.jpg","url":""},{"img":"https://www.1-dt.com/phone/img/yuan.jpg","url":""},{"img":"https://www.1-dt.com/phone/img/new.jpg","url":""},{"img":"https://www.1-dt.com/phone/img/zhong.png","url":""},{"img":"https://www.1-dt.com/phone/img/zhou.png","url":""}]
+console.log(data)
+
+class Discover extends Component {
+  render() {
+    return (
+      <div className="discoverContainer">
+        <div className="discoverTop"></div>
+        <div className="discoverList">
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+          <h1>123123</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Discover
