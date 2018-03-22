ddff7d7906588cd10ee0704ed37fb69d08442138
weifan she <923986234@qq.com>
2018-03-22 13:27:52 +0800

    use Login

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from '../Home/Home'
 import ProductDetail from '../ProductDetail/ProductDetail'
+import Login from '../Login/Login'
 
 // import './App.css'
 
@@ -12,6 +13,7 @@ class App extends Component {
         <div>
           <Home />
           <Route path="/product_detail/:id" component={ProductDetail}/>
+          <Route path="/login" component={Login}/>
         </div>
       </BrowserRouter>
     )
