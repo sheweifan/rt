b036cfc170c401dde3b590144da43489860cc4fc
dolor <923986234@qq.com>
2018-04-07 17:54:14 +0800

    fix postcss sourcemap error

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -181,6 +181,7 @@ module.exports = {
                   // Necessary for external CSS imports to work
                   // https://github.com/facebookincubator/create-react-app/issues/2677
                   ident: 'postcss',
+                  sourceMap: 'inline',
                   plugins: () => [
                     require('postcss-flexbugs-fixes'),
                     autoprefixer({
