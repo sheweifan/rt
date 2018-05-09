b14c468f31c64b8105753239375e45670814478a
dolor <923986234@qq.com>
2018-05-09 22:15:21 +0800

    add

--- src/action/user.js
+++ src/action/user.js
@@ -1,6 +1,14 @@
+import units from '../units'
+
 export const UPDATE_USERINFO = 'UPDATE_USERINFO'
 
-export const update_userinfo = userinfo => ({
-  type: UPDATE_USERINFO,
-  userinfo
-})
+export const update_userinfo = userinfo => {
+  units.localStorage.setItem('user', {
+    ...userinfo,
+    __time__: +new Date()
+  })
+  return {
+    type: UPDATE_USERINFO,
+    userinfo
+  }
+}
