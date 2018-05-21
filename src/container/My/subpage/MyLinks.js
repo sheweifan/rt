cd8cc0f9b654fe09e9b1961d76235c37c869cabd
dolor <923986234@qq.com>
2018-05-22 00:06:50 +0800

    add mylinks

---
+++ src/container/My/subpage/MyLinks.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react'
+import { List } from 'antd-mobile'
+import './MyLinks.styl'
+
+const Item = List.Item
+const dataList = [
+  { link: '', name: '存管账户', desc: '已开通' },
+  { link: '', name: '风险评测', desc: '稳健型' },
+  { link: '', name: '银行卡', desc: '更换绑卡' },
+  { link: '', name: '自动投标', desc: '未设置' },
+  { link: '', name: '我的推荐', desc: '' },
+  { link: '', name: '我的礼券', desc: '' },
+  { link: '', name: '我的奖金', desc: '' },
+  { link: '', name: 'VIP会员', desc: '' },
+  { link: '', name: '安全中心', desc: '' },
+  { link: '', name: '债券转让', desc: '' }
+]
+
+class MyLinks extends PureComponent {
+  render() {
+    return(
+      <List
+        className="myList"
+      >
+         {
+           dataList.map(item => (
+            <Item
+              key={item.name}
+              arrow="horizontal"
+              extra={item.desc || ''}
+            >
+              { item.name }
+            </Item>
+          ))
+        }
+      </List>
+    )
+  }
+}
+
+export default MyLinks
