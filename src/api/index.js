b2b9da14650fb80b521259311f266d4a0a9b42fc
dolor <923986234@qq.com>
2018-05-25 20:52:30 +0800

    add

--- src/api/index.js
+++ src/api/index.js
@@ -11,7 +11,7 @@ const post = (url: string, data?: Object = {}): Function => {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    // withCredentials: true,
+    withCredentials: true,
     transformRequest: [function (data) {
       let ret = ''
       for (let it in data) {
