34e9b468a7ec2d4a5a21ce0b31d6ad646e05d2fb
dolor <923986234@qq.com>
2018-06-01 00:37:59 +0800

    bind data

--- src/container/My/subpage/MyTop.js
+++ src/container/My/subpage/MyTop.js
@@ -3,21 +3,26 @@ import './MyTop.styl'
 
 class MyTop extends Component{
   render() {
+    const {
+      Assetmoney,
+      lunboSumGain,
+      balance
+    } = this.props
     return [
       <div key="1" className="myContainerCounts">
         <p className="nameAndBill">
           <span className="name">佘炜帆</span>
           <span className="bill">账单</span>
         </p>
-        <p className="count">5,4000.72<em>元</em></p>
+        <p className="count">{Assetmoney}<em>元</em></p>
         <p className="desc">资产总额</p>
-        <p className="count">1,400.72<em>元</em></p>
+        <p className="count">{lunboSumGain}<em>元</em></p>
         <p className="desc">累积收益</p>
       </div>,
       <div key="2" className="myContainerBalanceAndControl">
         <div className="balance">
           <p className="tips">可用余额</p>
-          <p className="count">910.02<em>元</em></p>
+          <p className="count">{balance}<em>元</em></p>
         </div>
         <div className="control">
           <span className="btn present">提现</span>
