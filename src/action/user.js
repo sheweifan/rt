078f6bec19383cb1ed2a88d9d132bc3981a10f71
dolor <923986234@qq.com>
2018-06-02 00:04:56 +0800

    update action

--- src/action/user.js
+++ src/action/user.js
@@ -1,14 +1,17 @@
-import units from '../units'
-
+// import units from '../units'
+import { myUser } from 'api'
 export const UPDATE_USERINFO = 'UPDATE_USERINFO'
 
 export const update_userinfo = userinfo => {
-  units.localStorage.setItem('user', {
-    ...userinfo,
-    __time__: +new Date()
-  })
-  return {
-    type: UPDATE_USERINFO,
-    userinfo
+  // units.localStorage.setItem('user', {
+  //   ...userinfo,
+  //   __time__: +new Date()
+  // })
+  return dispatch => {
+    return myUser()
+      .then(userinfo => dispatch({
+        type: UPDATE_USERINFO,
+        userinfo
+      }))
   }
 }
