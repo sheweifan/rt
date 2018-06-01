82a4654a1a33ed9d8f9cbf26f636b2258296be57
dolor <923986234@qq.com>
2018-06-01 20:22:58 +0800

    bind data

--- src/container/My/My.js
+++ src/container/My/My.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react'
 import { Redirect, withRouter } from 'react-router-dom'
-import { connect, } from 'react-redux'
-import { myUser } from 'api'
+import { connect } from 'react-redux'
+import { Toast } from 'antd-mobile'
 import MyTop from './subpage/MyTop'
 import MyList from './subpage/MyLinks'
+import { update_userinfo } from 'action/user'
 
 @connect(
-  state => ({ user: state.user })
+  state => ({ user: state.user }),
+  dispatch => ({
+    updateUserinfo: (state) => dispatch(update_userinfo(state))
+  })
 )
 @withRouter
 class My extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
   componentDidMount() {
-    myUser().then(res => {
-      console.log(res)
-    })
+    Toast.loading('loading...')
+    this.props.updateUserinfo()
+      .then(() => {
+        this.setState({
+          loading: false
+        })
+        Toast.hide()
+      })
   }
   render() {
-    if (this.props.user == null) {
+    const { user } = this.props
+    const { loading } = this.state
+    if (loading) {
+      return null
+    }
+    if (Number(user.result) !== 0) {
       return <Redirect to={{
         pathname: '/login',
         state: {
@@ -26,8 +46,8 @@ class My extends Component {
     }
     return (
       <div className="myContainer">
-        <MyTop />
-        <MyList />
+        <MyTop {...user}/>
+        <MyList {...user}/>
       </div>
     )
   }
