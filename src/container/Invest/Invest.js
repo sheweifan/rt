81404b4e4cd19f4833a54420c08bb2a9e36f72a4
dolor <923986234@qq.com>
2018-06-08 20:28:12 +0800

    add

--- src/container/Invest/Invest.js
+++ src/container/Invest/Invest.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import InnerContainer from 'component/InnerContainer/InnerContainer'
+import TitleTab from './subpage/TitleTab'
+import './Invest.styl'
 
 @withRouter
 class Invest extends Component {
@@ -8,13 +10,21 @@ class Invest extends Component {
     const { history } = this.props
     return (
       <InnerContainer
-      title="累计投资"
+      title={<TitleTab />}
       navBarConfig={{
         onLeftClick: history.goBack.bind(null, -1)
       }}
     >
       <div className="invest">
-        invest
+        <div className="investCountMessage">
+          <div className="all">
+            累计投资:<em className="high">105,429.00</em>元
+          </div>
+          <ul className="descs">
+            <li>散标投资85,429.00元</li>
+            <li>优选投资20,000.00元</li>
+          </ul>
+        </div>
       </div>
     </InnerContainer>
     )
