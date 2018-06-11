de9239dfed80ab0a8cc70dd5fafd089cfa4ef3fc
dolor <923986234@qq.com>
2018-06-12 00:06:15 +0800

    bind data

--- src/container/Invest/Invest.js
+++ src/container/Invest/Invest.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
+import { Toast } from 'antd-mobile'
+import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import InnerContainer from 'component/InnerContainer/InnerContainer'
 import TitleTab from './subpage/TitleTab'
+import { update_userinfo } from 'action/user'
 import './Invest.styl'
 
+@connect(
+  state => ({ user: state.user }),
+  dispatch => ({
+    updateUserinfo: (state) => dispatch(update_userinfo(state))
+  })
+)
 @withRouter
 class Invest extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loading: true
+    }
+  }
+  componentDidMount() {
+    Toast.loading('loading...')
+    this.props.updateUserinfo()
+      .then(() => {
+        this.setState({
+          loading: false
+        })
+        Toast.hide()
+      })
+  }
   render() {
-    const { history } = this.props
+    const { history, user } = this.props
+
     return (
       <InnerContainer
       title={<TitleTab />}
@@ -15,17 +42,24 @@ class Invest extends Component {
         onLeftClick: history.goBack.bind(null, -1)
       }}
     >
-      <div className="invest">
-        <div className="investCountMessage">
-          <div className="all">
-            累计投资:<em className="high">105,429.00</em>元
-          </div>
-          <ul className="descs">
-            <li>散标投资85,429.00元</li>
-            <li>优选投资20,000.00元</li>
-          </ul>
-        </div>
-      </div>
+      {
+        user
+        ? (
+            <div className="invest">
+              <div className="investCountMessage">
+                <div className="all">
+                  累计投资: <em className="high">{ user.sumMoney }</em> 元
+                </div>
+                <ul className="descs">
+                  <li>散标投资 { user.shanbiao } 元</li>
+                  <li>优选投资 { user.youjihua } 元</li>
+                </ul>
+              </div>
+            </div>
+          )
+        : null
+      }
+
     </InnerContainer>
     )
   }
